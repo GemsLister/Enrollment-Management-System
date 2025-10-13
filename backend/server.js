0c1d66566044e97423a1e2d5c7bed5badd444d2d
GemsLister <jameslester.l.lopez@gmail.com>
2025-10-14 00:39:48 +0800

    feat: intall cors

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import cors from "cors";
 // Mongo DB
 import connectDB from "./config/db.js";
 
 // Routes
-import signUpRoute from "./routes/signUpRoute.js";
+import userRoute from "./routes/userRoute.js";
 
 dotenv.config();
-connectDB();
 const app = express();
+connectDB();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
-app.use("/api/signUp", signUpRoute);
+app.use("/api/", userRoute); // For sign-up and login
 
 app.listen(process.env.PORT || 5000, () =>
   console.log(`Server running: ${process.env.PORT || 5000}`)
